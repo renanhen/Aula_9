385805524354a73399458f07392cbaebcffcfbb9
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-25 21:40:44 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+while True:
+    try:
+        numero = int(input("Digite um número inteiro (0 sai): "))
+        if numero == 0:
+            break
+    except Exception:
+            print("Valor inválido! Digite novamente.")
+
+
+while True:
+        numero = int(input("Digite um número inteiro (0 sai): "))
+        if numero == 0:
+            break
